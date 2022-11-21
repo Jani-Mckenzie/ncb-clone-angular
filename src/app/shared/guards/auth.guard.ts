b2536a0c2b9b6974aea378fe355c6713ff0c2276
Jani Mckenzie <janimckenzie@yahoo.com>
2022-11-21 10:53:11 -0500

    changes

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -3,7 +3,7 @@ import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanLoad, Route, 
 import { Observable } from 'rxjs';
 import Swal from 'sweetalert2';
 import { AuthService } from '../services/auth.service';
-import Swal from 'sweetalert2';
+
 
 
 @Injectable({
